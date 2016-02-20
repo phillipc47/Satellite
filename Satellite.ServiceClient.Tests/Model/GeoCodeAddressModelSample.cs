75b1c729f21534ce8afe3d1d5016cd871bfd9afa
phillipc47 <phil@server.fake>
2016-02-20 08:14:08 -0600

    Simple Model for Payload

---
+++ Satellite.ServiceClient.Tests/Model/GeoCodeAddressModelSample.cs
@@ -0,0 +1,51 @@
+﻿using Satellite.ServiceClient.Model;
+
+namespace Satellite.ServiceClient.Tests.Model
+{
+	internal static class GeoCodeAddressModelSample
+	{
+		public static string StringSample;
+		public static GeoCodeAddressModel.Envelope TypedSample;
+
+		static GeoCodeAddressModelSample()
+		{
+			StringSample = @"<?xml version=""1.0"" encoding=""utf-8""?>
+				<Envelope xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://schemas.xmlsoap.org/soap/envelope/"">
+					<Body>
+						<GeocodeAddressNonParsed xmlns=""https://geoservices.tamu.edu/"">
+							<streetAddress>123 Main Street</streetAddress>
+							<city>someCity</city>
+							<state>Texas</state>
+							<zip>99011</zip>
+							<apiKey>SomeAPIKey</apiKey>
+							<version>1.1</version>
+							<shouldCalculateCensus>true</shouldCalculateCensus>
+							<censusYear>2010</censusYear>
+							<shouldReturnReferenceGeometry>true</shouldReturnReferenceGeometry>
+							<shouldNotStoreTransactionDetails>true</shouldNotStoreTransactionDetails>
+						</GeocodeAddressNonParsed>
+					</Body>
+				</Envelope>";
+
+			TypedSample = new GeoCodeAddressModel.Envelope
+			{
+				Body = new GeoCodeAddressModel.EnvelopeBody
+				{
+					GeocodeAddressNonParsed = new GeoCodeAddressModel.GeocodeAddressNonParsed()
+					{
+						apiKey = "SomeAPIKey",
+						censusYear = "2010",
+						city = "someCity",
+						shouldCalculateCensus = "true",
+						shouldNotStoreTransactionDetails = "true",
+						shouldReturnReferenceGeometry = "true",
+						state = "Texas",
+						streetAddress = "123 Main Street",
+						version = "1.1",
+						zip = "99011"
+					}
+				}
+			};
+		}
+	}
+}
---
+++ Satellite.ServiceClient/Model/GeoCodeAddressModel.cs
@@ -0,0 +1,77 @@
+﻿namespace Satellite.ServiceClient.Model
+{
+	public class GeoCodeAddressModel
+	{
+		[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+		[System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.xmlsoap.org/soap/envelope/", IsNullable = false)]
+		public class Envelope
+		{
+			public EnvelopeBody Body { get; set; }
+		}
+
+
+		[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+		public class EnvelopeBody
+		{
+			[System.Xml.Serialization.XmlElementAttribute(Namespace = "https://geoservices.tamu.edu/")]
+			public GeocodeAddressNonParsed GeocodeAddressNonParsed { get; set; }
+		}
+
+
+		[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "https://geoservices.tamu.edu/")]
+		[System.Xml.Serialization.XmlRootAttribute(Namespace = "https://geoservices.tamu.edu/", IsNullable = false)]
+		public class GeocodeAddressNonParsed
+		{
+			private string internalShouldCalculateCensus;
+			private string internalShouldReturnReferenceGeometry;
+			private string internalShouldNotStoreTransactionDetails;
+
+			private bool isValidBoolean(string value)
+			{
+				bool result;
+				return bool.TryParse(value, out result);
+			}
+
+			private string scrubBoolean(string value)
+			{
+				return isValidBoolean(value) ? value : string.Empty;
+			}
+
+			public GeocodeAddressNonParsed()
+			{
+				censusYear = "2010";
+				shouldNotStoreTransactionDetails = "false";
+				shouldCalculateCensus = "false";
+				shouldReturnReferenceGeometry = "false";
+				version = "4.01";
+			}
+
+			public string streetAddress { get; set; }
+			public string city { get; set; }
+			public string state { get; set; }
+			public string zip { get; set; }
+			public string apiKey { get; set; }
+			public string version { get; set; }
+
+			public string shouldCalculateCensus
+			{
+				get { return internalShouldCalculateCensus; }
+				set { internalShouldCalculateCensus = scrubBoolean(value); }
+			}
+
+			public string censusYear { get; set; }
+
+			public string shouldReturnReferenceGeometry
+			{
+				get { return internalShouldReturnReferenceGeometry; }
+				set { internalShouldReturnReferenceGeometry = scrubBoolean(value); }
+			}
+
+			public string shouldNotStoreTransactionDetails
+			{
+				get { return internalShouldNotStoreTransactionDetails; }
+				set { internalShouldNotStoreTransactionDetails = scrubBoolean(value); }
+			}
+		}
+	}
+}
